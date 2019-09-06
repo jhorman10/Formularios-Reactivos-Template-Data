3780790cdc77f3e117d3734127f7d0bc9f3f9163
Jhorman Orozco <jsojhor@gmail.com>
2019-09-05 21:20:13 -0500

    formulario por data cancelado

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styleUrls: ['./data.component.css']
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  constructor() { }
+  forma: FormGroup;
 
-  ngOnInit() {
-  }
+  constructor(private formBuilder: FormBuilder) {
+
+    this.forma = this.formBuilder.group({
+      nombre: ['', [ Validators.required, Validators.minLength(3) ]],
+      apellido: ['', [ Validators.required, Validators.minLength(3) ]],
+      email: ['', [ Validators.required, Validators.email ]]
+    });
+
+   }
+
+   get f() { return this.forma.controls; }
+
+   guardarCambios() {
+    console.log(this.forma);
+    console.log(this.forma.value);
+   }
 
 }
